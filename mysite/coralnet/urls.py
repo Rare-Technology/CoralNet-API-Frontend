aa681e606ec790e3f8fc1006f5211cae90661a74
ibidyouadu <angel.d.umana@gmail.com>
2022-06-22 15:22:48 -0400

    add link to automatically generated tmp dropbox folders. add a success page after submitting POST

--- mysite/coralnet/urls.py
+++ mysite/coralnet/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'coralnet'
 
 urlpatterns = [
-    path('', views.home, name = 'home')
+    path('', views.home, name = 'home'),
+    path('success/', views.success, name = 'success')
 ]
--- mysite/coralnet/views.py
+++ mysite/coralnet/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.conf import settings
+import dropbox
+import tempfile
 
 # Create your views here.
 def home(request):
-    print(settings.STATIC_URL, settings.STATIC_ROOT)
-    return render(request, 'coralnet/home.html', {})
+    if request.method == 'POST':
+        redirect('success')
+    else:
+        dbx = dropbox.Dropbox(settings.DBX_ACCESS_TOKEN)
+        tmp_name = tempfile.TemporaryDirectory().name.split('/')[-1]
+        dbx_dir = dbx.file_requests_create(tmp_name, f'/{tmp_name}')
+
+        ctx = {
+            "dbx_link": dbx_dir.url
+        }
+
+    return render(request, 'coralnet/home.html', ctx)
+
+def success(request):
+    render(request, 'coralnet/succes.html', {})
